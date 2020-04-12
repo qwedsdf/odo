8675c14795fce0b6f252e70db816b822c2ab3de7
seiya972 <seiya9728@gmail.com>
2020-04-12 16:44:45 +0900

    ・卍一覧に4枚画像を表示出来るようにした。 ・卍するから4枚画像をツイート出来るようにした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,26 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post=Post.new(user_id: @current_user.id, content: params[:post][:content], tag: params[:post][:tag],image_1:"#{SecureRandom.uuid}.jpg")
-        if params[:input_image_1]
-            image = params[:input_image_name_1]
-            File.binwrite("public/images/tweet_image/#{@post.image_1}", image.read)           
+        @post=Post.new(user_id: @current_user.id, content: params[:post][:content], tag: params[:post][:tag])
+        if params[:post][:input_image_name_1]
+            image = params[:post][:input_image_name_1]
+            @post.image_1="#{SecureRandom.uuid}.jpg"
+            File.binwrite("public/images/tweet_images/#{@post.image_1}", image.read)           
+        end
+        if params[:post][:input_image_name_2]
+            image = params[:post][:input_image_name_2]
+            @post.image_2="#{SecureRandom.uuid}.jpg"
+            File.binwrite("public/images/tweet_images/#{@post.image_2}", image.read)           
+        end
+        if params[:post][:input_image_name_3]
+            image = params[:post][:input_image_name_3]
+            @post.image_3="#{SecureRandom.uuid}.jpg"
+            File.binwrite("public/images/tweet_images/#{@post.image_3}", image.read)           
+        end
+        if params[:post][:input_image_name_4]
+            image = params[:post][:input_image_name_4]
+            @post.image_4="#{SecureRandom.uuid}.jpg"
+            File.binwrite("public/images/tweet_images/#{@post.image_4}", image.read)           
         end
         if @post.save
             flash[:notice]="せーの！卍まんじ卍"
