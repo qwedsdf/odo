e40248aea955ccb4e619d9e0f05ebc387c8df3b7
yoha <yohayohayohayohayoha@gmail.com>
2020-04-26 22:50:33 +0900

    主絵師

--- db/migrate/20200418072012_create_notifications.rb
+++ db/migrate/20200418072012_create_notifications.rb
@@ -1,9 +1,9 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
-      t.integer :user_id
+      t.integer :post_user_id
       t.integer :post_id
-      t.boolean :is_looked :default: false, null: false
+      t.boolean :is_looked, default: false, null: false
 
       t.timestamps
     end
